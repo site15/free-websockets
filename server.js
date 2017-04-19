e90543979fbf4a884a1e19ed49ee5e04f0ba1842
endy <admin@site15.ru>
2017-04-19 19:56:22 +0500

    change code

--- server.js
+++ server.js
@@ -16,6 +16,21 @@ const io = socketIO(server);
 io.on('connection', (socket) => {
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
+  socket.on('message', function (receiver, data, messageId) {
+	if (!receiver || !data || !messageId){
+		return;
+	}
+	if (socket.io.connected[receiver]) {
+		socket.io.connected[receiver].emit('message', data);
+		if (socket.io.connected[socket.id]) {
+			socket.io.connected[socket.id].emit('message:'+messageId, true);
+		}
+	}else{
+		if (socket.io.connected[socket.id]) {
+			socket.io.connected[socket.id].emit('message:'+messageId, false);
+		}
+	}
+  });
 });
 
 setInterval(() => io.emit('time', new Date().toTimeString()), 5*60*1000);
