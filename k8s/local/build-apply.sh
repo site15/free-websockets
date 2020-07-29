c342b20bb47385db94dd81684614b81d7a2bae49
ILshat Khamitov <admin@site15.ru>
2020-07-29 19:50:05 +0500

    feat: update names

--- k8s/local/build-apply.sh
+++ k8s/local/build-apply.sh
@@ -1,6 +1,6 @@
 export UID=$(id -u)
 export GID=$(id -g)
 docker-compose -f ./k8s/local/docker-compose.config-yml build
-docker save endykaufman/free-websockets:local > ./tmp/endykaufman-free-websockets-local.tar
-microk8s ctr image import ./tmp/endykaufman-free-websockets-local.tar
+docker save free-websockets/server_service:local > ./tmp/server_service_local.tar
+microk8s ctr image import ./tmp/server_service_local.tar
 ./k8s/local/apply.sh
